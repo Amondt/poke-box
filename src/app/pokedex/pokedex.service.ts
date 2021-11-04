5c3672854674d40e3fc1db08f9a7ecc29c7155a4
Alex M <mondt.alexandre@gmail.com>
2021-11-04 01:38:48 +0100

    split reduced list in 2 parts in firebase

--- src/app/pokedex/pokedex.service.ts
+++ src/app/pokedex/pokedex.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@angular/core';
 import { ReducedPokemon } from '../shared/reducedPokemon.model';
 import { Subject } from 'rxjs';
 import { FirebaseService } from '../shared/firebase.service';
-import { Timestamp, onSnapshot } from 'firebase/firestore';
+import {
+    Timestamp,
+    onSnapshot,
+    query,
+    collection,
+    QuerySnapshot,
+    where,
+} from 'firebase/firestore';
 
 import { PokeapiWrapperService } from '../shared/pokeapi-js-wrapper.service';
 
@@ -11,9 +18,13 @@ export class PokedexService {
     private pokemonList: ReducedPokemon[] = [];
     pokemonListUpdated = new Subject<ReducedPokemon[]>();
 
-    pokemonListRef = this.firebaseService.createFirebaseRef(
+    pokemonListFirstPartRef = this.firebaseService.createFirebaseRef(
         'pokedex',
-        'pokemonReducedList'
+        'pokemonReducedListFirstPart'
+    );
+    pokemonListSecondPartRef = this.firebaseService.createFirebaseRef(
+        'pokedex',
+        'pokemonReducedListSecondPart'
     );
     pokedexConfigRef = this.firebaseService.createFirebaseRef(
         'pokedex',
@@ -85,92 +96,116 @@ export class PokedexService {
     updateFirebaseDb = (pokemonsFinalList: any[]) => {
         console.log('updating firebase');
 
+        const reducedPokemonList = pokemonsFinalList.map((pokemon: any) => {
+            const names = pokemon.species.names
+                .filter((name: any) => {
+                    return (
+                        name.language.name === 'fr' ||
+                        name.language.name === 'en' ||
+                        name.language.name === 'de' ||
+                        name.language.name === 'ja'
+                    );
+                })
+                .map((name: any) => {
+                    return {
+                        name: name.name,
+                        languageCode: name.language.name,
+                    };
+                });
+
+            const icon = pokemon.sprites.versions['generation-viii'].icons
+                .front_default
+                ? pokemon.sprites.versions['generation-viii'].icons
+                      .front_default
+                : pokemonsFinalList.find(
+                      (singlePokemon) =>
+                          singlePokemon.species.name === pokemon.species.name
+                  ).sprites.versions['generation-viii'].icons.front_default;
+
+            const stats = pokemon.stats.map((stat: any) => {
+                return {
+                    base_stat: stat.base_stat,
+                    name: stat.stat.name,
+                };
+            });
+            const types = pokemon.types.map((type: any) => {
+                return type.type.name;
+            });
+
+            const abilities = pokemon.abilities.map((ability: any) => {
+                return ability.ability.name;
+            });
+
+            const moves = pokemon.moves.map((move: any) => {
+                return move.move.name;
+            });
+
+            return {
+                id: pokemon.id,
+                order: pokemon.species.order,
+                name: pokemon.name,
+                names,
+                form: pokemon.forms.form_name,
+                generation: pokemon.species.generation.name,
+                icon,
+                stats,
+                types,
+                abilities,
+                moves,
+                isDefault: pokemon.is_default,
+                isBattleOnly: pokemon.forms.is_battle_only,
+                isMega: pokemon.forms.is_mega,
+                isBaby: pokemon.species.is_baby,
+                isLegendary: pokemon.species.is_legendary,
+                isMythical: pokemon.species.is_mythical,
+            };
+        });
+
         this.firebaseService.setFirebaseDoc(this.pokedexConfigRef, {
             lastUpdated: Timestamp.now(),
         });
 
-        this.firebaseService.setFirebaseDoc(this.pokemonListRef, {
-            list: pokemonsFinalList.map((pokemon: any) => {
-                const names = pokemon.species.names
-                    .filter((name: any) => {
-                        return (
-                            name.language.name === 'fr' ||
-                            name.language.name === 'en' ||
-                            name.language.name === 'de' ||
-                            name.language.name === 'ja'
-                        );
-                    })
-                    .map((name: any) => {
-                        return {
-                            name: name.name,
-                            languageCode: name.language.name,
-                        };
-                    });
-
-                const icon = pokemon.sprites.versions['generation-viii'].icons
-                    .front_default
-                    ? pokemon.sprites.versions['generation-viii'].icons
-                          .front_default
-                    : pokemonsFinalList.find(
-                          (singlePokemon) =>
-                              singlePokemon.species.name ===
-                              pokemon.species.name
-                      ).sprites.versions['generation-viii'].icons.front_default;
-
-                const stats = pokemon.stats.map((stat: any) => {
-                    return {
-                        base_stat: stat.base_stat,
-                        name: stat.stat.name,
-                    };
-                });
-                const types = pokemon.types.map((type: any) => {
-                    return type.type.name;
-                });
-
-                return {
-                    id: pokemon.id,
-                    order: pokemon.species.order,
-                    name: pokemon.name,
-                    names,
-                    form: pokemon.forms.form_name,
-                    generation: pokemon.species.generation.name,
-                    icon,
-                    stats,
-                    types,
-                    isDefault: pokemon.is_default,
-                    isBattleOnly: pokemon.forms.is_battle_only,
-                    isMega: pokemon.forms.is_mega,
-                    isBaby: pokemon.species.is_baby,
-                    isLegendary: pokemon.species.is_legendary,
-                    isMythical: pokemon.species.is_mythical,
-                };
-            }),
+        this.firebaseService.setFirebaseDoc(this.pokemonListFirstPartRef, {
+            list: reducedPokemonList.slice(0, 500),
         });
 
-        pokemonsFinalList.forEach((pokemon: any) => {
-            this.firebaseService.setFirebaseDoc(
-                this.firebaseService.createFirebaseRef(
-                    'pokedex',
-                    'pokemonList',
-                    'pokemons',
-                    pokemon.name
-                ),
-                pokemon
-            );
+        this.firebaseService.setFirebaseDoc(this.pokemonListSecondPartRef, {
+            list: reducedPokemonList.slice(500, reducedPokemonList.length),
         });
+
+        // pokemonsFinalList.forEach((pokemon: any) => {
+        //     this.firebaseService.setFirebaseDoc(
+        //         this.firebaseService.createFirebaseRef(
+        //             'pokedex',
+        //             'pokemonList',
+        //             'pokemons',
+        //             pokemon.name
+        //         ),
+        //         pokemon
+        //     );
+        // });
     };
 
     realtimeDbListen = () => {
-        const realtimeDbSub = onSnapshot(this.pokemonListRef, (doc) => {
-            if (doc.exists()) {
-                this.pokemonList = doc
-                    .data()
-                    .list.sort((a: ReducedPokemon, b: ReducedPokemon) =>
-                        a.order < b.order ? -1 : 1
-                    );
-                this.pokemonListUpdated.next(this.pokemonList.slice());
-                console.log('Data changed', this.pokemonList);
-            }
+        const q = query(
+            collection(this.firebaseService.db, 'pokedex'),
+            where('__name__', 'in', [
+                'pokemonReducedListFirstPart',
+                'pokemonReducedListSecondPart',
+            ])
+        );
+        const realtimePokemonListSub = onSnapshot(q, (QuerySnapshot) => {
+            let pokemonList: ReducedPokemon[] = [];
+            QuerySnapshot.forEach((doc) => {
+                pokemonList.push(...doc.data().list);
+            });
+            pokemonList = pokemonList.sort(
+                (a: ReducedPokemon, b: ReducedPokemon) =>
+                    a.order < b.order ? -1 : 1
+            );
+            this.pokemonList = pokemonList;
+            this.pokemonListUpdated.next(this.pokemonList.slice());
+            console.log('data changed', this.pokemonList);
         });
     };
 
